99b7af5242b60656f5371d476023dd31c11670cc
AnishDhawan <37919107+AnishDhawan@users.noreply.github.com>
2021-04-06 20:52:55 -0700

    helloios

---
+++ HelloIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloIOS
+//
+//  Created by Anish Dhawan on 4/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Go Hawks!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloIOS/HelloIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloIOSApp.swift
+//  HelloIOS
+//
+//  Created by stlp on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
